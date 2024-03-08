b005311754fab01189199508979405d732474734
matt-sanpedro <m.sanpedro@tcs.com>
2024-03-08 13:05:30 -0800

    Added introduction notes

---
+++ p-01-intro.py
@@ -0,0 +1,11 @@
+# in Pandas, data is organized in tabular data known as a Pandas DataFrame
+# it is recommenmded and optimal to have one datatype per column, different datatype in column can restrict functionality and cause severe problems
+
+"""
+ChatGPT: saving time coding
+
+- proficient python/pandas coder
+- can help to bugfix, improve and write code
+- does not replace coding skills, requires human oversight and intervention
+- can analyze code and provide help
+"""
