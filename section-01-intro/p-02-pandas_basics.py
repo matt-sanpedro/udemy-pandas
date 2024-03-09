4670b51d788de8366696d3f7a71de532912fd5d8
matt-sanpedro <m.sanpedro@tcs.com>
2024-03-08 16:55:12 -0800

    Added additional examples of attributes and pandas methods

--- section-01-intro/p-02-pandas_basics.py
+++ section-01-intro/p-02-pandas_basics.py
@@ -13,3 +13,17 @@ print(round(titanic))
 
 # min function will return column name at alphabetical pov
 print(min(titanic)) # age
+
+# ATTRIBUTES (stored in the object)
+print(titanic.shape) # (rows, cols)
+print(titanic.size) # all elements in dataframe (rows * cols)
+print(titanic.index)
+print(titanic.columns)
+
+# DATAFRAME METHODS
+print(titanic.head(n = 2))
+titanic.info()
+print(titanic.min(numeric_only=True))
+
+# METHOD CHAINING
+print(titanic.mean(numeric_only=True).sort_values())
