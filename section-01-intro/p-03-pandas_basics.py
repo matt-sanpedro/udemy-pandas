2d0581cca9ac8c189f15e6c164137499864e7fd1
matt-sanpedro <m.sanpedro@tcs.com>
2024-03-15 11:49:04 -0700

    Added additional examples on column selection with dot notation

--- section-01-intro/p-03-pandas_basics.py
+++ section-01-intro/p-03-pandas_basics.py
@@ -7,7 +7,16 @@ pd_series = titanic["age"]
 print(pd_series) # output -> pandas series
 print(type(pd_series)) # output: class 'pandas.core.series.Series'
 
-# select multiple columns
+# select multiple columns (create a list)
+# most methods are available for both series and dataframes
 print(type(titanic["age"]))             # datatype: series
 print(type(titanic[["age", "sex"]]))    # datatype: dataframe
 print(titanic[["age", "sex", "fare"]])
+
+# alternative method select one column with dot notation
+print(titanic.age)
+
+# equals test whether to objects have same elements
+# some people think should NOT use dot notation but can perform with less code
+print(titanic.age.equals(titanic["age"]))
+print(titanic.embarked)
