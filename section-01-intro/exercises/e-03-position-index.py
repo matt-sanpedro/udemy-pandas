49ae4eb6e5d6a495e08b51f23558a3659eab1856
mobius <mattsanpedro7@gmail.com>
2024-05-12 15:39:53 -0700

    Added problem statement and learned column selecting more than available will NOT throw error

--- section-01-intro/exercises/e-03-position-index.py
+++ section-01-intro/exercises/e-03-position-index.py
@@ -1,7 +1,15 @@
 import pandas as pd
 df = pd.read_csv("titanic.csv")
+"""
+Select and save the last 10 rows (and only the first two columns)
+in the variable last_rows
+"""
 
 # print(df.iloc[2, 3])
 
 last_rows = df.iloc[-10:,:2]
 print(last_rows)
+
+# investigate syntax
+print("--- INVESTIGATE ---")
+print(df.iloc[-10:,:5])
