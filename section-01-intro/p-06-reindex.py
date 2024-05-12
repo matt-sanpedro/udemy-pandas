be564d4060036f299afc2be5b22b67bf944c9365
mobius <mattsanpedro7@gmail.com>
2024-05-11 18:35:30 -0700

    Organized code examples and added additional method rename for renaming columns

--- section-01-intro/p-06-reindex.py
+++ section-01-intro/p-06-reindex.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-summer = pd.read_csv("summer.csv", index_col="Athlete")
-# summer = pd.read_csv("summer.csv")
+# summer = pd.read_csv("summer.csv", index_col="Athlete")
+# # cannot use reindex method with duplicate indices
+# print(summer.reindex(["PHELPS, Michael"], columns = ["Medal", "Age"]))
 
-# print(summer.loc[[0, 5, 30000], ["Athlete", "Medal"]])
+# reindex method: conforms DataFrame to new index with optional filling logic
+summer = pd.read_csv("summer.csv")
+print(summer.loc[[0, 5, 30000], ["Athlete", "Medal"]])
+new_df = summer.reindex(index = [0, 5, 30000, 40000], columns = ["Athlete", "Medal", "Age"])
+print(new_df)
 
-# print(summer.reindex(index = [0, 5, 30000, 40000], columns = ["Athlete", "Medal", "Age"]))
-
-# cannot use reindex method with duplicate indices
-print(summer.reindex(["PHELPS, Michael"], columns = ["Medal", "Age"]))
+# can use rename method to rename columns
+new_df.rename(columns={"Athlete": "AthleteName"}, inplace=True)
+print(new_df)
