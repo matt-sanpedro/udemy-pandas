0056dc435a61b4c3f12f20bb746588dfb1e9a060
mobius <mattsanpedro7@gmail.com>
2024-05-12 20:34:00 -0700

    Added notes and additional example on reindex method on duplicate axis, will throw error

--- section-01-intro/p-06-reindex.py
+++ section-01-intro/p-06-reindex.py
@@ -6,10 +6,19 @@ import pandas as pd
 
 # reindex method: conforms DataFrame to new index with optional filling logic
 summer = pd.read_csv("summer.csv")
+
+# warning: loc method will throw error if index is out of range
 print(summer.loc[[0, 5, 30000], ["Athlete", "Medal"]])
+
+# best practice: if in doubt of index number, reindex will NOT throw error
+# note: age values will return NaN since it was nonexistent before
 new_df = summer.reindex(index = [0, 5, 30000, 40000], columns = ["Athlete", "Medal", "Age"])
 print(new_df)
 
 # can use rename method to rename columns
 new_df.rename(columns={"Athlete": "AthleteName"}, inplace=True)
 print(new_df)
+
+# # warning: cannot index with duplicate axis, below will throw error
+# summer_athlete_df = pd.read_csv("summer.csv", index_col="Athlete")
+# summer_athlete_df.reindex(index=["PHELPS, Michael"], columns=["Medal", "Age"])
