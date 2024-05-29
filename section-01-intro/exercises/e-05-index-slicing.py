525bd5d1d3205e2dd7d4ddeed70049ae7dfb51f6
mobius <mattsanpedro7@gmail.com>
2024-05-28 23:28:05 -0700

    Added more examples with same solution

--- section-01-intro/exercises/e-05-index-slicing.py
+++ section-01-intro/exercises/e-05-index-slicing.py
@@ -15,6 +15,7 @@ print(cars["name"][393])
 print(cars.iloc[100])
 
 # select name column on index 200 with iloc method
+print(cars.iloc[200, 8])
 print(cars.iloc[200]["name"])
 
 # test for equality
@@ -24,6 +25,8 @@ print(cars.tail(10).equals(cars.iloc[-10:])) # True
 
 # Select the last 10 rows and the columns mpg, horsepower, origin and name with the iloc operator
 print(cars.iloc[-10:][["mpg", "horsepower", "origin", "name"]])
+print(cars.iloc[-10:, [0, 3, 7, 8]])
+
 
 # import dataset and set column name as index
 cars = pd.read_csv("cars.csv", index_col="name")
@@ -38,6 +41,7 @@ print(cars.loc["ford ranger"])
 
 # select the row with the index label "ford torino" and only the columns "model_year" and "origin"
 print(cars.loc[["ford torino"], ["model_year", "origin"]])
+print(cars.loc["ford torino", ["model_year", "origin"]])
 
 # select the columns "mpg" and "weight" (all rows) with the loc operator!
 print(cars.loc[:, ["mpg", "weight"]])
