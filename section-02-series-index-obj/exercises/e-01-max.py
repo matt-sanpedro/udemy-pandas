44f5ce7abe5aa54a4e1593cc99156b0f0efc1db3
mobius <mattsanpedro7@gmail.com>
2024-06-30 16:56:40 -0700

    Added additional examples to cover course material

--- section-02-series-index-obj/exercises/e-01-max.py
+++ section-02-series-index-obj/exercises/e-01-max.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
 df = pd.read_csv("titanic.csv")
+print(df)
 
 # print highest value in fare column
 print(df.fare.max())
 
 # most common/frequent ticket price
 print(df.fare.value_counts().index[0])
+
+# number of unique fare costs
+print(df.fare.unique())
+print(len(df.fare.unique()))
+print(df.fare.nunique())
+print(df.fare.describe())
